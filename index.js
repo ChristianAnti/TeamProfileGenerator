e3ed8a01dee04b909c75412f9a9c7d00b5764c61
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2021-11-09 23:34:44 -0500

    BUG FIXXING AND MORE

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-// the requires for the different roles
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
-const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
 const teamArr = []
 
-// various npm requires etc.
-const inquirer = require("inquirer");
-const jest = require("jest");
-const render = require("./lib/htmlRenderer")
-const path = require('path');
-const fs = require('fs');
-
-// path requires 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html")
-
-
 function employeeQs() {
-    inquirer.prompt([
-        {
+
+    // base questions all roles will be using
+    inquirer.prompt([{
             type: "input",
             message: "Please enter the teammate's name.",
             name: "answerName"
@@ -41,7 +36,8 @@ function employeeQs() {
             name: "answerRole",
             choices: ["Engineer", "Intern", "Manager"]
         },
-    ]).then(function(answers){
+    ]).then(function (answers) {
+
         if (answers.answerRole === "Engineer") {
             engineerQs(answers);
         } else if (answers.answerRole === "Intern") {
@@ -50,15 +46,13 @@ function employeeQs() {
             managerQs(answers);
         }
     })
-};
+}
 
-
-function managerQs(baseAnswers) {
-    inquirer.prompt ([
-        {
+function engineerQs(baseAnswers) {
+    inquirer.prompt([{
             type: "input",
-            message: "Please enter the manager's office number.",
-            name: "answerOfficeNumber",
+            message: "Please enter the engineer's GitHub username.",
+            name: "answerGithub",
         },
         {
             type: "confirm",
@@ -66,44 +60,22 @@ function managerQs(baseAnswers) {
             name: "answerAddAnother",
         },
     ]).then(function (answers) {
-        const newManager = new Manager(baseAnswers.answerName, baseAnswers.answerID, baseAnswers.answerEmail, answers.answerGitub)
-        teamArr.push(newManager);
-        if (answers.answerAddAnother === true) {
-            employeeQs()
-        } else {
-            buildTeam();
-            console.log("Manager rendered !!!")
-        }
-    })
-};
-
-function engineerQs(baseAnswers) {
-    inquirer.prompt ([
-        {
-            type: "input",
-            message: "Please enter the engineer's GitHub username.",
-            name: "answerGithub",
-        },
-        {
-            type: "confirm",
-            message: "Please enter the engineer's GitHub username if they have another.",
-            name: "answerAddAnother",
-        },
-    ]).then(function (answers) {
-        const newEngineer = new Engineer(baseAnswers.answerName, baseAnswers.answerID, baseAnswers.answerEmail, answers.answerGitub)
+        const newEngineer = new Engineer(baseAnswers.answerName, baseAnswers.answerID, baseAnswers.answerEmail, answers.answerGithub);
         teamArr.push(newEngineer);
         if (answers.answerAddAnother === true) {
+
             employeeQs()
         } else {
+
             buildTeam();
-            console.log("Engineer rendered !!!")
+            console.log("The boys are TOGETHER!")
         }
     })
-};
+}
+
 
 function internQs(baseAnswers) {
-    inquirer.prompt ([
-        {
+    inquirer.prompt([{
             type: "input",
             message: "Please enter the intern's School.",
             name: "answerSchool",
@@ -114,24 +86,49 @@ function internQs(baseAnswers) {
             name: "answerAddAnother",
         },
     ]).then(function (answers) {
-        const newIntern = new Intern(baseAnswers.answerName, baseAnswers.answerID, baseAnswers.answerEmail, answers.answerSchool)
+        const newIntern = new Intern(baseAnswers.answerName, baseAnswers.answerID, baseAnswers.answerEmail, answers.answerSchool);
         teamArr.push(newIntern);
         if (answers.answerAddAnother === true) {
+
             employeeQs()
         } else {
+
             buildTeam();
-            console.log("Intern rendered !!!")
+            console.log("The boys are TOGETHER!!")
         }
     })
-};
+}
 
+function managerQs(baseAnswers) {
+    inquirer.prompt([{
+            type: "input",
+            message: "Please enter the manager's office number.",
+            name: "answerOfficeNumber",
+        },
+        {
+            type: "confirm",
+            message: "Please enter any more info, if aplicable.",
+            name: "answerAddAnother",
+        },
+    ]).then(function (answers) {
+        const newManager = new Manager(baseAnswers.answerName, baseAnswers.answerID, baseAnswers.answerEmail, answers.answerOfficeNumber);
+        teamArr.push(newManager);
+        if (answers.answerAddAnother === true) {
+
+            employeeQs()
+        } else {
+
+            buildTeam();
+            console.log("The boys are TOGETHER!!!")
+        }
+    })
+}
 
 function buildTeam() {
-    if(!fs.existsSync(OUTPUT_DIR)) {
+    if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
     }
-    fs.writeFileSync(outputPath, render(teamArr), "utf-8")
-
-};
+    fs.writeFileSync(outputPath, render(teamArr), "utf-8");
+}
 
 employeeQs();
